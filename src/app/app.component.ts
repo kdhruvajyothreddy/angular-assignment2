636347ac2e3cfc02b733560dbfd2d12caa54e52c
kdhruvajyothreddy <kdhruvajyothreddy@gmail.com>
2020-11-30 21:57:53 +0530

    Using ngFor to detect change to add and remove object from list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,15 @@ export class AppComponent {
     {id: 3, name: 'course3'},
     {id: 4, name: 'course4'}
   ]; // Created a list of objects to render each object
+
+  onAdd() {
+    this.courses.push({id: 5, name: 'course5'});
+    // To append the course object to the list to be added on the front end
+  }
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+    //  To remove the course object that was passed to this method from the list of objects on front end
+  }
 }
