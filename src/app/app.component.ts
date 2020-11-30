2f283f167cd8dc9a4ded924f5f9a8d106ba31072
kdhruvajyothreddy <kdhruvajyothreddy@gmail.com>
2020-11-30 22:08:49 +0530

    ngFor to automatically update the DOM object's name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,9 +18,8 @@ export class AppComponent {
     // To append the course object to the list to be added on the front end
   }
 
-  onRemove(course) {
-    let index = this.courses.indexOf(course);
-    this.courses.splice(index, 1);
-    //  To remove the course object that was passed to this method from the list of objects on front end
+  onChange(course) {
+    course.name="UPDATED!"
+    // Using the passed the course object to update the name of the object to be automatically updated by Angular
   }
 }
