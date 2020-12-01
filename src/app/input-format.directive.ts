b87da62bf630eef18aaa4abeac3fdf8bbe1b069b
kdhruvajyothreddy <kdhruvajyothreddy@gmail.com>
2020-12-01 23:14:21 +0530

    Using custom directive to manipulate value on front end using property binding

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,17 +1,26 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
 
+  @Input('appInputFormat') format;
+  // Created an input property 'appInputFormat' to be used on front end
+
   constructor(private el: ElementRef) { }
+  // Created an element to access DOM Element using ElementRef wrapper
   
   @HostListener('blur') onBlur() {
     console.log("On Blur");  
     let value: string = this.el.nativeElement.value;
-    this.el.nativeElement.value = value.toLowerCase();
+    // Assigning the native element to access DOM element and assign it to a value 'value'
+
+    this.el.nativeElement.value = (this.format == 'lowercase') ? value.toLowerCase() : value.toUpperCase();
+    // Assigning the manipulated 'value' conditionally based on the value assigned to 'format' on front end to 'value' which has direct access to DOM elements
+    
   }
+
 
 
   
