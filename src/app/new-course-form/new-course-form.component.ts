53674ae9a8353871b53fe1b268d4857338f4d37f
kdhruvajyothreddy <kdhruvajyothreddy@gmail.com>
2021-01-02 20:14:09 +0530

    Reactive forms: Using FormArray to push property values

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'new-course-form',
@@ -7,15 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCourseFormComponent {
 
-  categoryTypes =[
-    {id: 1, name: "Development"},
-    {id: 2, name: "Art"},
-    {id: 3, name: "Languages"}
-  ]
+  form = new FormGroup({
+    topics: new FormArray([]) // Creating a FormFroup where an instance of FormArray is created, as we will be passing an array of objects
+  });
 
-  submit(courseForm) {
-    console.log(courseForm);
-    
+  addTopic(topic: HTMLInputElement){  // Declaring the expecting value "topic" as an "HTMLInputElement" property
+    this.topics.push(new FormControl(topic.value)); // Pushing the passed value to the FormArray
+    topic.value = ''; // To clear value entered on the input field after the value is pushed into the array
+  }
+
+  get topics() {
+    return this.form.get('topics') as FormArray; 
+  }
+  // Creating a get method to return the "topics" property as a Form Array
+
+  removeTopic(topic: FormControl) {
+    let index = this.topics.controls.indexOf(topic); // Gathering the index of the "topics" property
+    this.topics.removeAt(index); // Removing the specific topics property from the array based on the index value
   }
 
 }
